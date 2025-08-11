340b2349fb237f5cd9c1c8cc15520a13a151d5ce
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-08-11 21:27:38 +0530

    Update Footer.tsx

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       {/* Your component content here */}
 
       <footer className="mx-auto max-w-7xl py-10 text-center text-sm text-slate-500">
-        &copy; 2024. Created by @Srija
+        &copy; 2025. Created by @PremKumar
       </footer>
     </>
   );
